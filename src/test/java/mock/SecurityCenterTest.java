80c771fa699c5e7e076a1ad6d01b49055906698d
LiYuan1229 <823260355@qq.com>
2020-09-16 01:31:27 +0800

    [LiYuan]:finish mock work

--- src/test/java/mock/SecurityCenterTest.java
+++ src/test/java/mock/SecurityCenterTest.java
@@ -2,18 +2,42 @@ package mock;
 
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class SecurityCenterTest {
     /* 需求描述：
     编写SecurityCenter类的单元测试，单元测试switchOn方法，不依赖于DoorPanel的close的方法实现
     * */
 
+     private static class DoorPanelMock extends DoorPanel{
+       private boolean flag=false;
+
+        public void close() {
+            flag = true;
+        }
+
+        public boolean isClosedBeCalled(){
+            return flag;
+        }
+
+    }
+     private SecurityCenter securityCenter;
+     private DoorPanelMock doorPanelMock;
+
     @BeforeEach
     public void setUp() {
+         doorPanelMock = new DoorPanelMock();
+         securityCenter = new SecurityCenter(doorPanelMock);
     }
 
     @Test
     public void shouldVerifyDoorIsClosed() {
+        securityCenter.switchOn();
+
+        assertTrue(doorPanelMock.isClosedBeCalled());
+
     }
 }
--- src/test/java/stub/GradeServiceTest.java
+++ src/test/java/stub/GradeServiceTest.java
@@ -1,15 +1,28 @@
 package stub;
 
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class GradeServiceTest {
     /* 需求描述：
     编写GradeService类的单元测试，单元测试calculateAverageGrades方法
     * */
+    class GradeStubs extends GradeSystem{
 
+        public List<Double> gradesFor(long studentId) {
+
+            return Arrays.asList(80.0, 90.0, 100.0); // 只是一个例子
+        }
+
+    }
     @Test
     public void shouldReturn90WhenCalculateStudentAverageGradeAndGradeIs80And90And100() {
-        //Assertions.assertEquals(90.0, result);
+        GradeStubs gradeStubs = new GradeStubs();
+        List<Double> result = gradeStubs.gradesFor(161008);
+        Assertions.assertEquals(90.0, result);
     }
 }
